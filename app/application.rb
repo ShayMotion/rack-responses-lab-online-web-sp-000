24f1b181e087719a9406da78c2f77e63330879fd
Shayan Mohsin <shaymotionfilms@gmail.com>
2019-11-23 02:47:59 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,15 +1,16 @@
-class App
- 
+class Application
+
   def call(env)
     resp = Rack::Response.new
-    
-    if num_1==num_2 && num_2==num_3
+
+    if Time.now.hour.between?(0, 11)
       resp.write "Good Morning!"
+    elsif Time.now.hour.between?(12, 17)
+      resp.write "Good Afternoon."
     else
-      resp.write "Good Afternoon!"
+      resp.write "Good Evening!"
     end
-    
+
     resp.finish
   end
- 
 end
